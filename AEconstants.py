4e29d7053fd87818ed0fa5bd82206d06d2776627
ofirbarak <ofirbirka@gmial.com>
2018-11-23 17:17:19 +0200

    works

--- AEconstants.py
+++ AEconstants.py
@@ -5,14 +5,14 @@ INPUT_CH = 1 if GRAY else 3 # set automatically by GRAY parameter
 
 
 # architecture params
-KERNELS_SIZE = [[5,5, INPUT_CH, 32], [3, 3, 32, 64]]  # HWIO - height, width, input_ch, output_ch
+KERNELS_SIZE = [[5,5, INPUT_CH, 4], [3, 3, 4, 4]]  # HWIO - height, width, input_ch, output_ch
 NLAYER_SIZES = [[64, 64],[64,64]]#, [15,15], [8,8], [4,4]]
 WINDOW = [4, 1]
 STRIDE = [4, 1]
 
 
 # learning params
-batch_size = 1
+batch_size = 2
 NUM_LAYERS = 2
 
 LEARNING_RATE = 0.001
